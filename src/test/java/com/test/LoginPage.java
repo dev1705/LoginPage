6c54d46cbffd48935eb9724ee59d368a1aa70f45
dev1705 <devarajgandhigopal@gmail.com>
2022-01-20 23:57:47 +0530

    testCase1

--- src/test/java/com/test/LoginPage.java
+++ src/test/java/com/test/LoginPage.java
@@ -1,5 +1,11 @@
 package com.test;
 
 public class LoginPage {
-
+	private void testCase1() {
+System.out.println("Abc@123");
+	}
+public static void main(String[] args) {
+	LoginPage loginPage =new LoginPage();
+	loginPage.testCase1();
+}
 }
